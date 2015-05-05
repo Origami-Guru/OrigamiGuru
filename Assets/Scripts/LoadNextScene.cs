834b55d15487e985c6b5612b1fba8172eef9251b
Chayangkul <nut_nutvorn@hotmail.com>
2015-05-05 14:51:03 +0700

    add manual fox fish crane

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextScene : MonoBehaviour {
+	public void LoadNextStepFish1()
+	{
+		Application.LoadLevel("Fish01");
+	}
+	public void LoadNextStepFish2()
+	{
+		Application.LoadLevel("Fish02");
+	}
+	public void LoadNextStepFish3()
+	{
+		Application.LoadLevel("Fish03");
+	}
+	public void LoadNextStepFish4()
+	{
+		Application.LoadLevel("Fish04");
+	}
+	public void LoadNextStepFish5()
+	{
+		Application.LoadLevel("Fish05");
+	}
+
+	public void LoadNextStepFox1()
+	{
+		Application.LoadLevel("Fox01");
+	}
+	public void LoadNextStepFox2()
+	{
+		Application.LoadLevel("Fox02");
+	}
+	public void LoadNextStepFox3()
+	{
+		Application.LoadLevel("Fox03");
+	}
+	public void LoadNextStepFox4()
+	{
+		Application.LoadLevel("Fox04");
+	}
+	public void LoadNextStepFox5()
+	{
+		Application.LoadLevel("Fox05");
+	}
+	public void LoadNextStepFox6()
+	{
+		Application.LoadLevel("Fox06");
+	}
+	public void LoadNextStepFox7()
+	{
+		Application.LoadLevel("Fox07");
+	}
+	public void LoadNextStepFox8()
+	{
+		Application.LoadLevel("Fox08");
+	}
+	
+	public void LoadNextStepCrane1()
+	{
+		Application.LoadLevel("Crane01");
+	}
+	public void LoadNextStepCrane2()
+	{
+		Application.LoadLevel("Crane02");
+	}
+	public void LoadNextStepCrane3()
+	{
+		Application.LoadLevel("Crane03");
+	}
+	public void LoadNextStepCrane4()
+	{
+		Application.LoadLevel("Crane04");
+	}
+	public void LoadNextStepCrane5()
+	{
+		Application.LoadLevel("Crane05");
+	}
+	public void LoadNextStepCrane6()
+	{
+		Application.LoadLevel("Crane06");
+	}
+	public void LoadNextStepCrane7()
+	{
+		Application.LoadLevel("Crane07");
+	}
+	public void LoadNextStepCrane8()
+	{
+		Application.LoadLevel("Crane08");
+	}
+	public void LoadNextStepCrane9()
+	{
+		Application.LoadLevel("Crane09");
+	}
+	public void LoadNextStepCrane10()
+	{
+		Application.LoadLevel("Crane10");
+	}
+	public void LoadNextStepCrane11()
+	{
+		Application.LoadLevel("Crane11");
+	}
+	public void LoadNextStepCrane12()
+	{
+		Application.LoadLevel("Crane12");
+	}
+	public void LoadNextStepCrane13()
+	{
+		Application.LoadLevel("Crane13");
+	}
+
+}
