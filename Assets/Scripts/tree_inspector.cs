e7fab5fa93a731313d690df0a312731ce2852beb
Chayangkul <nut_nutvorn@hotmail.com>
2015-05-26 19:30:33 +0700

    test code to disable game object in tree_inspector.cs

---
+++ Assets/Scripts/tree_inspector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tree_inspector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public GameObject Trees;
+	// Update is called once per frame
+	void Update () {
+	//	if(Input.GetKey(KeyCode.Space)){
+			//Trees.SetActive(false);
+	//	}
+	//	else{
+	//		Trees.SetActive(true);
+	//	}
+
+		GameObject targetObject;
+		GameObject targetObject2;
+		targetObject = GameObject.Find ("cat_step_01_01");
+		targetObject2 = GameObject.Find ("cat_step_01_02");
+	
+		 
+}
+}
+
