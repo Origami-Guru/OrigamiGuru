c75e2bcdee03c189a2bab889cacf0533218853ff
Chayangkul <nut_nutvorn@hotmail.com>
2015-05-30 15:54:07 +0700

    add Util.cs

--- Assets/Facebook/Scripts/MainMenuFB.cs
+++ Assets/Facebook/Scripts/MainMenuFB.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
-
+using System.Collections.Generic;
 public class MainMenuFB : MonoBehaviour {
-
+	public GameObject UIFBLoggedIn;
+	public GameObject UIFBNotLoggedIn;
+	//public GameObject UIFBAvatar;
+	public GameObject UIFBUserName;
+	private Dictionary<string, string> profile = null;
 	// Use this for initialization
-	void Start () {
-
-	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 	private void SetInit(){
 		Debug.Log ("SetInit");
-		enabled = true;
+	
 		if (FB.IsLoggedIn) {
-			Debug.Log ("Already logged in");
 
+			Debug.Log ("Already logged in");
+			
 		} else {
-			FBLogin();
+			DealWithFBMenu(true);
+			//FBLogin();
 		}
 	}
 	private void OnHideUnity(bool isGameShown){
@@ -37,10 +37,10 @@ public class MainMenuFB : MonoBehaviour {
 		FB.Init(SetInit, OnHideUnity);  
 	}
 
-	void FBLogin(){
+	public void FBLogin(){
 	            
-		FB.Login ("email,publish_actions , user_about_me,user_birthday", LoginCallback);                                                        
-		                                                                                                          
+		FB.Login ("publish_actions", LoginCallback);                                                        
+		                                                                                                  
 		   
 	}
 	void LoginCallback(FBResult result)                                                        
@@ -48,15 +48,55 @@ public class MainMenuFB : MonoBehaviour {
 		Debug.Log("LoginCallback");                                                          
 		
 		if (FB.IsLoggedIn) {                                                                                      
-			Debug.Log ("FB login worked");   
-			OnLoggedIn();   
+			Debug.Log ("FB login worked");
+			Debug.Log(FB.UserId);
+			Debug.Log(FB.AccessToken);
+			DealWithFBMenu(true);
+			//OnLoggedIn();   
 		} else
 			Debug.Log ("FB loing Fail");
+			DealWithFBMenu(false);
 	}                                                                                          
 	void OnLoggedIn()                                                                          
 	{                                                                                          
 		Debug.Log("Logged in. ID: " + FB.UserId);                                            
 	}  
-	       
+	void DealWithFBMenu(bool IsLoggedIn){
+		if(IsLoggedIn){
+			UIFBLoggedIn.SetActive (false);
+			UIFBNotLoggedIn.SetActive(true);
 
+			//get profile picture code
+			//FB.API(Util.GetPictureURL("me",128,128),Facebook.HttpMethod.GET,DealWithProfilePicture);
+			FB.API("/me?fields=id,first_name", Facebook.HttpMethod.GET, DealWithUsername);
+			//get username code
+		}
+		else{
+			UIFBLoggedIn.SetActive(true);
+			UIFBNotLoggedIn.SetActive(false);
+		}
+	}  
+
+	void DealWithUsername(FBResult result){
+		if (result.Error !=null) {
+			Debug.Log ("Problem with getting Username");
+			//FB.API(Util.GetPictureURL("me",128,128),Facebook.HttpMethod.GET,DealWithProfilePicture);
+			FB.API("/me?fields=id,first_name", Facebook.HttpMethod.GET, DealWithUsername);
+			return;
+
+		}
+		profile = Util.DeserializeJSONProfile(result.Text); 
+		Text Usermsg = UIFBUserName.GetComponent<Text> ();
+		Usermsg.text = "Helle," + profile ["first_name"];
+	}
+	public void ShareWithFriends (){
+			FB.Feed(
+			linkCaption:"i'm OrigamiGuru",
+			picture:"https://photos-3.dropbox.com/t/2/AABN8t808wJxFo6cDi6c8L73y6xmjcAf182FaDJmnrQVWQ/12/22580284/jpeg/32x32/1/1432915200/0/2/pigeon_step_07_16.jpg/CLyY4gogASACIAMgBCAFIAYgBygBKAI/BEWaH1EVplmAs2e-iVlCxmyrzOV9C82mSvk2N-RIN0k?size=1024x768&size_mode=2",
+			link:"https://photos-3.dropbox.com/t/2/AABN8t808wJxFo6cDi6c8L73y6xmjcAf182FaDJmnrQVWQ/12/22580284/jpeg/32x32/1/1432915200/0/2/pigeon_step_07_16.jpg/CLyY4gogASACIAMgBCAFIAYgBygBKAI/BEWaH1EVplmAs2e-iVlCxmyrzOV9C82mSvk2N-RIN0k?size=1024x768&size_mode=2",
+			linkName:"Test Application OrgamiGuRU ProJeCt",
+			callback: LoginCallback
+			);
+		
+		}
 }
